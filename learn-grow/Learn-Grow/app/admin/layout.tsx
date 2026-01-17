1ae6a3c33c70971c4d32313b67196ba9676247cc
kazinayeem <mohammadalinayeem360@gmail.com>
2026-01-17 16:36:18 +0600

    fix: remove unused import statement for cleaner code

--- learn-grow/Learn-Grow/app/admin/layout.tsx
+++ learn-grow/Learn-Grow/app/admin/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import AdminSidebar from "@/components/admin/AdminSidebar";
 
 export default function AdminLayout({
   children,
